5a1d58a719afd750d55a2c5d17b821f7c47feb68
void <void@91177308-0d34-0410-b5e6-96231b3b80d8>
2007-11-02 05:41:07 +0000

    Creating llvm-1200-20 branch

---
+++ llvm/tags/Apple/llvm-1200-20/include/llvm/Bitcode/Serialize.h
@@ -0,0 +1,65 @@
+//==- Serialize.h - Generic Object Serialization to Bitcode -------*- C++ -*-=//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by Ted Kremenek and is distributed under the
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the interface for generic object serialization to
+// LLVM bitcode.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_BITCODE_SERIALIZE_OUTPUT
+#define LLVM_BITCODE_SERIALIZE_OUTPUT
+
+#include "llvm/Bitcode/Serialization.h"
+#include "llvm/Bitcode/BitstreamWriter.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/DenseMap.h"
+
+namespace llvm {
+
+class Serializer {
+  BitstreamWriter& Stream;
+  SmallVector<uint64_t,10> Record;
+  bool inBlock;
+  
+  typedef DenseMap<const void*,unsigned> MapTy;
+  MapTy PtrMap;
+  
+public:
+  Serializer(BitstreamWriter& stream, unsigned BlockID = 0);
+  ~Serializer();
+  
+  template <typename T>
+  inline void Emit(const T& X) { SerializeTrait<T>::Emit(*this,X); }
+  
+  void EmitInt(unsigned X);
+  void EmitBool(bool X) { EmitInt(X); }
+  void EmitCStr(const char* beg, const char* end);
+  void EmitCStr(const char* cstr);
+  
+  void EmitPtr(const void* ptr) { EmitInt(getPtrId(ptr)); }
+  
+  template <typename T>
+  void EmitRef(const T& ref) { EmitPtr(&ref); }
+  
+  template <typename T>
+  void EmitOwnedPtr(T* ptr) {
+    EmitPtr(ptr);
+    if (ptr) SerializeTrait<T>::Emit(*this,*ptr);
+  }
+
+  void Flush() { if (inRecord()) EmitRecord(); }
+  
+private:
+  void EmitRecord();
+  inline bool inRecord() { return Record.size() > 0; }
+  unsigned getPtrId(const void* ptr);
+};
+
+} // end namespace llvm
+#endif
---
+++ llvm/tags/Apple/llvm-1200-20/lib/Bitcode/Writer/Serialize.cpp
@@ -0,0 +1,81 @@
+//==- Serialize.cpp - Generic Object Serialization to Bitcode ----*- C++ -*-==//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by Ted Kremenek and is distributed under the
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the internal methods used for object serialization.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Bitcode/Serialize.h"
+#include "string.h"
+
+using namespace llvm;
+
+Serializer::Serializer(BitstreamWriter& stream, unsigned BlockID)
+  : Stream(stream), inBlock(BlockID >= 8) {
+    
+  if (inBlock) Stream.EnterSubblock(8,3);
+}
+
+Serializer::~Serializer() {
+  if (inRecord())
+    EmitRecord();
+
+  if (inBlock)
+    Stream.ExitBlock();
+  
+  Stream.FlushToWord();
+}
+
+void Serializer::EmitRecord() {
+  assert(Record.size() > 0 && "Cannot emit empty record.");
+  Stream.EmitRecord(8,Record);
+  Record.clear();
+}
+
+void Serializer::EmitInt(unsigned X) {
+  Record.push_back(X);
+}
+
+void Serializer::EmitCStr(const char* s, const char* end) {
+  Record.push_back(end - s);
+  
+  while(s != end) {
+    Record.push_back(*s);
+    ++s;
+  }
+
+  EmitRecord();
+}
+
+void Serializer::EmitCStr(const char* s) {
+  EmitCStr(s,s+strlen(s));
+}
+
+unsigned Serializer::getPtrId(const void* ptr) {
+  if (!ptr)
+    return 0;
+  
+  MapTy::iterator I = PtrMap.find(ptr);
+  
+  if (I == PtrMap.end()) {
+    unsigned id = PtrMap.size()+1;
+    PtrMap[ptr] = id;
+    return id;
+  }
+  else return I->second;
+}
+
+#define INT_EMIT(TYPE)\
+void SerializeTrait<TYPE>::Emit(Serializer&S, TYPE X) { S.EmitInt(X); }
+
+INT_EMIT(bool)
+INT_EMIT(unsigned char)
+INT_EMIT(unsigned short)
+INT_EMIT(unsigned int)
+INT_EMIT(unsigned long)
