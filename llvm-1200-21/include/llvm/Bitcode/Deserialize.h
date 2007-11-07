b9b910b1728034ebe97f84b7e5930eadcc138839
void <void@91177308-0d34-0410-b5e6-96231b3b80d8>
2007-11-07 19:41:23 +0000

    Creating llvm-1200-21 branch

---
+++ llvm-1200-21/include/llvm/Bitcode/Deserialize.h
@@ -0,0 +1,196 @@
+//=- Deserialize.h - Generic Object Deserialization from Bitcode --*- C++ -*-=//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by Ted Kremenek and is distributed under the
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the interface for generic object deserialization from
+// LLVM bitcode.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_BITCODE_SERIALIZE_INPUT
+#define LLVM_BITCODE_SERIALIZE_INPUT
+
+#include "llvm/Bitcode/BitstreamReader.h"
+#include "llvm/Bitcode/Serialization.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/Allocator.h"
+#include "llvm/Support/DataTypes.h"
+#include <vector>
+
+namespace llvm {
+  
+class Deserializer {  
+
+  //===----------------------------------------------------------===//
+  // Internal type definitions.
+  //===----------------------------------------------------------===//
+  
+  struct BPNode {
+    BPNode* Next;
+    uintptr_t& PtrRef;
+    
+    BPNode(BPNode* n, uintptr_t& pref) 
+      : Next(n), PtrRef(pref) {
+        PtrRef = 0;
+      }
+  };
+  
+  struct BPEntry { 
+    union { BPNode* Head; void* Ptr; };
+    
+    BPEntry() : Head(NULL) {}
+    
+    static inline bool isPod() { return true; }
+    
+    void SetPtr(BPNode*& FreeList, void* P);    
+  };  
+  
+  class BPKey {
+    unsigned Raw;
+    
+  public:
+    BPKey(unsigned PtrId) : Raw(PtrId << 1) { assert (PtrId > 0); }
+    BPKey(unsigned code, unsigned) : Raw(code) {}
+    
+    void MarkFinal() { Raw |= 0x1; }
+    bool hasFinalPtr() const { return Raw & 0x1 ? true : false; }
+    unsigned getID() const { return Raw >> 1; }
+    
+    static inline BPKey getEmptyKey() { return BPKey(0,0); }
+    static inline BPKey getTombstoneKey() { return BPKey(1,0); }
+    static inline unsigned getHashValue(const BPKey& K) { return K.Raw & ~0x1; }
+
+    static bool isEqual(const BPKey& K1, const BPKey& K2) {
+      return (K1.Raw ^ K2.Raw) & ~0x1 ? false : true;
+    }
+    
+    static bool isPod() { return true; }
+  };
+  
+  typedef llvm::DenseMap<BPKey,BPEntry,BPKey,BPEntry> MapTy;
+
+  //===----------------------------------------------------------===//
+  // Internal data members.
+  //===----------------------------------------------------------===//
+  
+  BitstreamReader& Stream;
+  SmallVector<uint64_t,10> Record;
+  unsigned RecIdx;
+  BumpPtrAllocator Allocator;
+  BPNode* FreeList;
+  MapTy BPatchMap;  
+  
+  //===----------------------------------------------------------===//
+  // Public Interface.
+  //===----------------------------------------------------------===//
+  
+public:
+  Deserializer(BitstreamReader& stream);
+  ~Deserializer();
+
+  uint64_t ReadInt();
+  int64_t ReadSInt();
+  
+  bool ReadBool() {
+    return ReadInt() ? true : false;
+  }
+
+  template <typename T>
+  inline T& Read(T& X) {
+    SerializeTrait<T>::Read(*this,X);
+    return X;
+  }
+
+  template <typename T>
+  inline T* Materialize() {
+    return SerializeTrait<T>::Materialize(*this);
+  }
+  
+  char* ReadCStr(char* cstr = NULL, unsigned MaxLen=0, bool isNullTerm=true);
+  void ReadCStr(std::vector<char>& buff, bool isNullTerm=false);
+
+  template <typename T>
+  inline T* ReadOwnedPtr(bool AutoRegister = true) {
+    unsigned PtrId = ReadInt();
+
+    if (PtrId == 0)
+      return NULL;
+    
+    T* x = SerializeTrait<T>::Materialize(*this);
+    
+    if (AutoRegister)
+      RegisterPtr(PtrId,x);
+    
+    return x;
+  }
+  
+  template <typename T>
+  inline void ReadOwnedPtr(T*& Ptr, bool AutoRegister = true) {
+    Ptr = ReadOwnedPtr<T>(AutoRegister);
+  }
+  
+  template <typename T>
+  void ReadPtr(T*& PtrRef, bool AllowBackpatch = true) {
+    ReadUIntPtr(reinterpret_cast<uintptr_t&>(PtrRef), AllowBackpatch);
+  }
+  
+  template <typename T>
+  void ReadPtr(const T*& PtrRef, bool AllowBackpatch = true) {
+    ReadPtr(const_cast<T*&>(PtrRef), AllowBackpatch);
+  }
+  
+  template <typename T>
+  T* ReadPtr() { T* x; ReadPtr<T>(x,false); return x; }
+
+  void ReadUIntPtr(uintptr_t& PtrRef, bool AllowBackpatch = true);
+  
+  template <typename T>
+  T& ReadRef() {
+    T* p = reinterpret_cast<T*>(ReadInternalRefPtr());
+    return *p;
+  }
+
+  void RegisterPtr(unsigned PtrId, const void* Ptr);
+  
+  void RegisterPtr(const void* Ptr) {
+    RegisterPtr(ReadInt(),Ptr);
+  }
+  
+  bool AtEnd();
+
+  bool inRecord();
+private:
+  void ReadRecord();  
+  uintptr_t ReadInternalRefPtr();
+  
+  static inline bool HasFinalPtr(MapTy::value_type& V) {
+    return V.first.hasFinalPtr();
+  }
+  
+  static inline uintptr_t GetFinalPtr(MapTy::value_type& V) {
+    return reinterpret_cast<uintptr_t>(V.second.Ptr);
+  }
+  
+  static inline BPNode* GetBPNode(MapTy::value_type& V) {
+    return V.second.Head;
+  }
+    
+  static inline void SetBPNode(MapTy::value_type& V, BPNode* N) {
+    V.second.Head = N;
+  }
+  
+  void SetPtr(MapTy::value_type& V, const void* P) {
+    V.first.MarkFinal();
+    V.second.SetPtr(FreeList,const_cast<void*>(P));
+  }
+};
+    
+} // end namespace llvm
+
+#endif
---
+++ llvm-1200-21/include/llvm/Bitcode/Serialize.h
@@ -0,0 +1,70 @@
+//==- Serialize.h - Generic Object Serialization to Bitcode -------*- C++ -*-=//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by Ted Kremenek and is distributed under the
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the interface for generic object serialization to
+// LLVM bitcode.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_BITCODE_SERIALIZE_OUTPUT
+#define LLVM_BITCODE_SERIALIZE_OUTPUT
+
+#include "llvm/Bitcode/Serialization.h"
+#include "llvm/Bitcode/BitstreamWriter.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/DenseMap.h"
+
+namespace llvm {
+
+class Serializer {
+  BitstreamWriter& Stream;
+  SmallVector<uint64_t,10> Record;
+  unsigned BlockLevel;
+  
+  typedef DenseMap<const void*,unsigned> MapTy;
+  MapTy PtrMap;
+  
+public:
+  Serializer(BitstreamWriter& stream);
+  ~Serializer();
+  
+  template <typename T>
+  inline void Emit(const T& X) { SerializeTrait<T>::Emit(*this,X); }
+  
+  void EmitInt(uint64_t X);
+  void EmitSInt(int64_t X);
+  
+  void EmitBool(bool X) { EmitInt(X); }
+  void EmitCStr(const char* beg, const char* end);
+  void EmitCStr(const char* cstr);
+  
+  void EmitPtr(const void* ptr) { EmitInt(getPtrId(ptr)); }
+  
+  template <typename T>
+  void EmitRef(const T& ref) { EmitPtr(&ref); }
+  
+  template <typename T>
+  void EmitOwnedPtr(T* ptr) {
+    EmitPtr(ptr);
+    if (ptr) SerializeTrait<T>::Emit(*this,*ptr);
+  }
+
+  void FlushRecord() { if (inRecord()) EmitRecord(); }
+  
+  void EnterBlock(unsigned BlockID = 8, unsigned CodeLen = 3);
+  void ExitBlock();    
+  
+private:
+  void EmitRecord();
+  inline bool inRecord() { return Record.size() > 0; }
+  unsigned getPtrId(const void* ptr);
+};
+
+} // end namespace llvm
+#endif
