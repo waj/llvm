ebd3f7ee894326038faa30f0d5d5d62266f5a558
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-05-07 03:45:29 +0000

    Tagging checker-22.

---
+++ checker-22/tools/llvmc2/Action.h
@@ -0,0 +1,38 @@
+//===--- Tools.h - The LLVM Compiler Driver ---------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open
+// Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//  Action - encapsulates a single shell command.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TOOLS_LLVMC2_ACTION_H
+#define LLVM_TOOLS_LLVMC2_ACTION_H
+
+#include <string>
+#include <vector>
+
+namespace llvmc {
+
+  typedef std::vector<std::string> StringVector;
+
+  class Action {
+    std::string Command_;
+    std::vector<std::string> Args_;
+  public:
+    Action (const std::string& C,
+            const StringVector& A)
+      : Command_(C), Args_(A)
+    {}
+
+    int Execute() const;
+  };
+
+}
+
+#endif // LLVM_TOOLS_LLVMC2_ACTION_H
