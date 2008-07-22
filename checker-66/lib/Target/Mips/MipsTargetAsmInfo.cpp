e143e555576b5c3a86352ac444f71a967f2a65a6
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-07-22 00:47:11 +0000

    Tagging checker-66.

---
+++ checker-66/lib/Target/Mips/MipsTargetAsmInfo.cpp
@@ -0,0 +1,41 @@
+//===-- MipsTargetAsmInfo.cpp - Mips asm properties -------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the declarations of the MipsTargetAsmInfo properties.
+//
+//===----------------------------------------------------------------------===//
+
+#include "MipsTargetAsmInfo.h"
+#include "MipsTargetMachine.h"
+
+using namespace llvm;
+
+MipsTargetAsmInfo::MipsTargetAsmInfo(const MipsTargetMachine &TM):
+  ELFTargetAsmInfo(TM) {
+
+  AlignmentIsInBytes          = false;
+  COMMDirectiveTakesAlignment = true;
+  Data16bitsDirective         = "\t.half\t";
+  Data32bitsDirective         = "\t.word\t";
+  Data64bitsDirective         = NULL;
+  PrivateGlobalPrefix         = "$";
+  JumpTableDataSection        = "\t.rdata";
+  CommentString               = "#";
+  ReadOnlySection             = "\t.rdata";
+  ZeroDirective               = "\t.space\t";
+  BSSSection                  = "\t.section\t.bss";
+  LCOMMDirective              = "\t.lcomm\t";
+  CStringSection              = ".rodata.str";
+
+  if (!TM.getSubtarget<MipsSubtarget>().hasABICall())
+    JumpTableDirective = "\t.word\t";
+  else
+    JumpTableDirective = "\t.gpword\t";
+
+}
