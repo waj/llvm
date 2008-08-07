ed414a3e190eb886c1990ee4c40abe9a3f27bf6f
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-08-07 18:10:50 +0000

    Tagging checker-74.

---
+++ llvm/tags/checker/checker-74/lib/Target/Sparc/SparcTargetAsmInfo.cpp
@@ -0,0 +1,45 @@
+//===-- SparcTargetAsmInfo.cpp - Sparc asm properties -----------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the declarations of the SparcTargetAsmInfo properties.
+//
+//===----------------------------------------------------------------------===//
+
+#include "SparcTargetAsmInfo.h"
+
+using namespace llvm;
+
+SparcELFTargetAsmInfo::SparcELFTargetAsmInfo(const TargetMachine &TM):
+  ELFTargetAsmInfo(TM) {
+  Data16bitsDirective = "\t.half\t";
+  Data32bitsDirective = "\t.word\t";
+  Data64bitsDirective = 0;  // .xword is only supported by V9.
+  ZeroDirective = "\t.skip\t";
+  CommentString = "!";
+  ConstantPoolSection = "\t.section \".rodata\",#alloc\n";
+  COMMDirectiveTakesAlignment = true;
+}
+
+std::string SparcELFTargetAsmInfo::PrintSectionFlags(unsigned flags) const {
+  std::string Flags = ",";
+
+  if (flags & SectionFlags::Mergeable)
+    return ELFTargetAsmInfo::PrintSectionFlags(flags);
+
+  if (!(flags & SectionFlags::Debug))
+    Flags += "#alloc";
+  if (flags & SectionFlags::Code)
+    Flags += "#execinstr";
+  if (flags & SectionFlags::Writeable)
+    Flags += "#write";
+  if (flags & SectionFlags::TLS)
+    Flags += "#tls";
+
+  return Flags;
+}
