eeb2d2ffde144b0a6eaadf2de620b688ec8fb28e
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-08-13 03:55:40 +0000

    Tagging checker-76.

---
+++ llvm/tags/checker/checker-76/lib/Target/PowerPC/PPCTargetAsmInfo.h
@@ -0,0 +1,46 @@
+//=====-- PPCTargetAsmInfo.h - PPC asm properties -------------*- C++ -*--====//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the declaration of the DarwinTargetAsmInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef PPCTARGETASMINFO_H
+#define PPCTARGETASMINFO_H
+
+#include "llvm/Target/TargetAsmInfo.h"
+#include "llvm/Target/DarwinTargetAsmInfo.h"
+#include "llvm/Target/ELFTargetAsmInfo.h"
+
+namespace llvm {
+
+  // Forward declaration.
+  class PPCTargetMachine;
+
+  struct PPCTargetAsmInfo : public virtual TargetAsmInfo {
+    explicit PPCTargetAsmInfo(const PPCTargetMachine &TM);
+  };
+
+  struct PPCDarwinTargetAsmInfo : public PPCTargetAsmInfo,
+                                  public DarwinTargetAsmInfo {
+    explicit PPCDarwinTargetAsmInfo(const PPCTargetMachine &TM);
+    virtual unsigned PreferredEHDataFormat(DwarfEncoding::Target Reason,
+                                           bool Global) const;
+  };
+
+  struct PPCLinuxTargetAsmInfo : public PPCTargetAsmInfo,
+                                 public ELFTargetAsmInfo {
+    explicit PPCLinuxTargetAsmInfo(const PPCTargetMachine &TM);
+    virtual unsigned PreferredEHDataFormat(DwarfEncoding::Target Reason,
+                                           bool Global) const;
+  };
+
+} // namespace llvm
+
+#endif
