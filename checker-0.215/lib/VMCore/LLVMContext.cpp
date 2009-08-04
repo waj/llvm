2cec0175a810db90c522812c656988feec75eb98
kremenek <kremenek@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-08-04 17:06:19 +0000

    Tagging checker-0.215.

---
+++ checker-0.215/lib/VMCore/LLVMContext.cpp
@@ -0,0 +1,33 @@
+//===-- LLVMContext.cpp - Implement LLVMContext -----------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file implements LLVMContext, as a wrapper around the opaque
+// class LLVMContextImpl.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/LLVMContext.h"
+#include "llvm/Constants.h"
+#include "llvm/DerivedTypes.h"
+#include "llvm/Instruction.h"
+#include "llvm/Metadata.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "LLVMContextImpl.h"
+#include <cstdarg>
+
+using namespace llvm;
+
+static ManagedStatic<LLVMContext> GlobalContext;
+
+LLVMContext& llvm::getGlobalContext() {
+  return *GlobalContext;
+}
+
+LLVMContext::LLVMContext() : pImpl(new LLVMContextImpl(*this)) { }
+LLVMContext::~LLVMContext() { delete pImpl; }
