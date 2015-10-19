waj/llvm

commit d513587e1d74734ef8962597052b1e7033f8ecef

[Orc] Use '= default' for move constructor/assignment as per dblaikie's review.
Thanks Dave!

llvm-svn: 250749

include/llvm/ExecutionEngine/Orc/CompileOnDemandLayer.h
@@ -69,21 +69,12 @@ private:
 
     LogicalModuleResources() {}
 
-    LogicalModuleResources(LogicalModuleResources &&Other) {
-      SourceModule = std::move(Other.SourceModule);
-      StubsToClone = std::move(StubsToClone);
-      StubsMgr = std::move(StubsMgr);
-    }
-
     // Explicit move constructor to make MSVC happy.
-    LogicalModuleResources& operator=(LogicalModuleResources &&Other) {
-      SourceModule = std::move(Other.SourceModule);
-      StubsToClone = std::move(StubsToClone);
-      StubsMgr = std::move(StubsMgr);
-      return *this;
-    }
+    LogicalModuleResources(LogicalModuleResources &&Other) = default;
 
     // Explicit move assignment to make MSVC happy.
+    LogicalModuleResources& operator=(LogicalModuleResources &&Other) = default;
+
     JITSymbol findSymbol(StringRef Name, bool ExportedSymbolsOnly) {
       if (Name.endswith("$stub_ptr") && !ExportedSymbolsOnly) {
         assert(!ExportedSymbolsOnly && "Stubs are never exported");

commit 064c4ee46bf3359e9729e384743b1200c390056e

[Orc] Fix MSVC bugs introduced in r250749.

include/llvm/ExecutionEngine/Orc/CompileOnDemandLayer.h
@@ -67,13 +67,20 @@ private:
     std::set<const Function*> StubsToClone;
     std::unique_ptr<IndirectStubsMgrT> StubsMgr;
 
-    LogicalModuleResources() {}
+    LogicalModuleResources() = default;
 
     // Explicit move constructor to make MSVC happy.
-    LogicalModuleResources(LogicalModuleResources &&Other) = default;
+    LogicalModuleResources(LogicalModuleResources &&Other)
+        : SourceModule(std::move(Other.SourceModule)),
+          StubsToClone(std::move(Other.StubsToClone)),
+          StubsMgr(std::move(Other.StubsMgr)) {}
 
     // Explicit move assignment to make MSVC happy.
-    LogicalModuleResources& operator=(LogicalModuleResources &&Other) = default;
+    LogicalModuleResources& operator=(LogicalModuleResources &&Other) {
+      SourceModule = std::move(Other.SourceModule);
+      StubsToClone = std::move(Other.StubsToClone);
+      StubsMgr = std::move(Other.StubsMgr);
+    }
 
     JITSymbol findSymbol(StringRef Name, bool ExportedSymbolsOnly) {
       if (Name.endswith("$stub_ptr") && !ExportedSymbolsOnly) {